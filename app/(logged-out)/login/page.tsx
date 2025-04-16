e766d5f61b88affcf7f881fd85582552cf5dc240
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-16 16:27:15 +0300

    add card for login form

---
+++ app/(logged-out)/login/page.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <PersonStandingIcon size={50} />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login your SupportMe account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Login form</p>
+        </CardContent>
+        <CardFooter className="justify-between">
+          <small>Don`t have an account?</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default Page;
