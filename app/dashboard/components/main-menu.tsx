6666d1e0eca6b3b06db36e90b957769719ac32c2
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-17 12:33:54 +0300

    add sidebar menu items and footer

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,12 +1,34 @@
+import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
+import MenuItem from "./menu-item";
 import MenuTitle from "./menu-title";
+import Link from "next/link";
+import LightDarkToggle from "@/components/ui/light-dark-toggle";
 
 const MainMenu = () => {
   return (
-    <div className="bg-muted p-4 overflow-auto">
+    <nav className="md:bg-muted overflow-auto p-4 flex flex-col min-h-screen">
       <header className="hidden md:block border-b dark:border-b-black border-b-zinc-300 pb-4">
         <MenuTitle />
       </header>
-    </div>
+      <ul className="py-4 grow">
+        <MenuItem href="/dashboard">My dashboard</MenuItem>
+        <MenuItem href="/dashboard/teams">Teams</MenuItem>
+        <MenuItem href="/dashboard/employees">Employees</MenuItem>
+        <MenuItem href="/dashboard/account">Account</MenuItem>
+        <MenuItem href="/dashboard/settings">Settings</MenuItem>
+      </ul>
+      <footer className="flex gap-2 items-center">
+        <Avatar>
+          <AvatarFallback className="bg-pink-300 dark:bg-pink-800 rounded-full w-8 h-8 flex items-center justify-center text-xs font-medium">
+            TP
+          </AvatarFallback>
+        </Avatar>
+        <Link href="/" className="hover:underline">
+          Logout
+        </Link>
+        <LightDarkToggle className="ml-auto" />
+      </footer>
+    </nav>
   );
 };
 
--- app/dashboard/components/menu-item.tsx
+++ app/dashboard/components/menu-item.tsx
@@ -1,5 +1,30 @@
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const MenuItem = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  return (
+    <Link
+      className={cn(
+        "block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground",
+        isActive &&
+          "bg-primary hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground text-primary-foreground"
+      )}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
 };
 
 export default MenuItem;
