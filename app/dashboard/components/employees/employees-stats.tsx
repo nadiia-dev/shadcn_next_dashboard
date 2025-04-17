9fa92a2c0f657f723358731201a476096b072a01
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-17 12:48:29 +0300

    add dashboard tabs && employees cards

---
+++ app/dashboard/components/employees/employees-stats.tsx
@@ -0,0 +1,15 @@
+import { Card } from "@/components/ui/card";
+
+const EmployeesStats = () => {
+  return (
+    <>
+      <div className="grid lg:grid-cols-3 gap-4">
+        <Card>Card 1</Card>
+        <Card>Card 2</Card>
+        <Card>Card 3</Card>
+      </div>
+    </>
+  );
+};
+
+export default EmployeesStats;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,19 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import EmployeesStats from "./components/employees/employees-stats";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
+        <TabsTrigger value="employees">Employees</TabsTrigger>
+        <TabsTrigger value="teams">Teams</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">
+        <EmployeesStats />
+      </TabsContent>
+      <TabsContent value="teams">teams stats</TabsContent>
+    </Tabs>
+  );
 };
 
 export default Page;
