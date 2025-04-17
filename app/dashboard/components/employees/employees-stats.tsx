b2c89b5a4f266ffbe1971aeffd83ba3cbaf590b3
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-17 12:57:34 +0300

    add total employees card

--- app/dashboard/components/employees/employees-stats.tsx
+++ app/dashboard/components/employees/employees-stats.tsx
@@ -1,12 +1,39 @@
-import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { UserIcon } from "lucide-react";
+import Link from "next/link";
 
 const EmployeesStats = () => {
+  const totalEmployees = 100;
   return (
     <>
       <div className="grid lg:grid-cols-3 gap-4">
-        <Card>Card 1</Card>
-        <Card>Card 2</Card>
-        <Card>Card 3</Card>
+        <Card>
+          <CardHeader className="pb-2">
+            <CardTitle className="text-base">Total employees</CardTitle>
+          </CardHeader>
+          <CardContent className="flex justify-between">
+            <div className="flex gap-2">
+              <UserIcon />
+              <div className="text-5xl font-bold">{totalEmployees}</div>
+            </div>
+            <div>
+              <Button size="xs" asChild>
+                <Link href="/dashboard/employees">View all</Link>
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="pb-2">
+            <CardTitle className="text-base">Employees present</CardTitle>
+          </CardHeader>
+        </Card>
+        <Card className="border-pink-500 flex flex-col">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-base">Employee of the month</CardTitle>
+          </CardHeader>
+        </Card>
       </div>
     </>
   );
