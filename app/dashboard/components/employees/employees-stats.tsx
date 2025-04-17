b9d2d1972706578e364729769e77f35381132136
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-17 13:02:55 +0300

    add present employees card

--- app/dashboard/components/employees/employees-stats.tsx
+++ app/dashboard/components/employees/employees-stats.tsx
@@ -1,10 +1,28 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { UserIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertTriangleIcon,
+  BadgeCheckIcon,
+  LaptopIcon,
+  PartyPopperIcon,
+  UserCheck2Icon,
+  UserCheckIcon,
+  UserIcon,
+  UserRoundXIcon,
+} from "lucide-react";
 import Link from "next/link";
 
 const EmployeesStats = () => {
   const totalEmployees = 100;
+  const presentEmployees = 80;
+  const employeesPresentPercentage = (presentEmployees / totalEmployees) * 100;
+
   return (
     <>
       <div className="grid lg:grid-cols-3 gap-4">
@@ -28,6 +46,25 @@ const EmployeesStats = () => {
           <CardHeader className="pb-2">
             <CardTitle className="text-base">Employees present</CardTitle>
           </CardHeader>
+          <CardContent>
+            <div className="flex gap-2">
+              {presentEmployees > 75 ? <UserCheck2Icon /> : <UserRoundXIcon />}
+              <div className="text-5xl font-bold">{presentEmployees}</div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            {employeesPresentPercentage > 75 ? (
+              <span className="text-xs text-green-500 flex gap-1 items-center">
+                <BadgeCheckIcon />
+                {employeesPresentPercentage}% of employees are present
+              </span>
+            ) : (
+              <span className="text-xs text-red-500 flex gap-1 items-center">
+                <AlertTriangleIcon />
+                Only {employeesPresentPercentage}% of employees are present
+              </span>
+            )}
+          </CardFooter>
         </Card>
         <Card className="border-pink-500 flex flex-col">
           <CardHeader className="pb-2">
